2d471cd27a0926b93699ccc497fb8b7a64c64ea6
Shwetha Acharya <sacharya@redhat.com>
2022-07-13 15:06:04 +0530

    Update geo-rep_cleanup.sh

--- geo-rep_cleanup.sh
+++ geo-rep_cleanup.sh
@@ -5,7 +5,9 @@
 # geo-rep_cleanup.sh <secondary_ip1> <secondary_ip2> <secondary_ip3> <primary_ip2>
 # <primary_ip3> <primary_client_ip> <secondary_client_ip>
 #
-# primary_ip1 is not taken this script is to executed on the machine with primary_ip1
+# primary_ip1 is not taken as one of the parameters,
+# as this script is to be executed on a primary node and ip of the node
+# where the commands are executed is not necessary
 ############################################################################
 
 echo cleaning up geo-rep setup
@@ -24,11 +26,12 @@ yes | gluster volume stop primary
 yes | gluster volume delete primary
 rm -rf /brick1
 ssh $primary_ip2 rm -rf /brick2
-ssh $primary_ip3 -rf /brick3
+ssh $primary_ip3 rm -rf /brick3
 
 echo cleaning up secondary volume and bricks
 yes | ssh $secondary_ip1 gluster volume stop secondary
 yes | ssh $secodary_ip2 gluster volume delete secondary
+
 yes | ssh $secondary_ip1 rm -rf /brick*
 yes | ssh $secondary_ip2 rm -rf /brick*
 yes | ssh $secondary_ip3 rm -rf /brick*
